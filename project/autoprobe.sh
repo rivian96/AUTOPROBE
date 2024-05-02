c28759b668012eaf9f7eac5c2844d5685de5f994
R1V14N <119740534+rivian96@users.noreply.github.com>
2024-05-02 20:28:33 +0530

    Update autoprobe.sh

--- project/autoprobe.sh
+++ project/autoprobe.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-#script to run volatility plugins
 
-echo -e "\e[36m
+
+echo -e "\e[35m
 ░█████╗░██╗░░░██╗████████╗░█████╗░██████╗░██████╗░░█████╗░██████╗░███████╗
 ██╔══██╗██║░░░██║╚══██╔══╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝
 ███████║██║░░░██║░░░██║░░░██║░░██║██████╔╝██████╔╝██║░░██║██████╦╝█████╗░░
@@ -10,44 +10,72 @@ echo -e "\e[36m
 ██║░░██║╚██████╔╝░░░██║░░░╚█████╔╝██║░░░░░██║░░██║╚█████╔╝██████╦╝███████╗
 ╚═╝░░╚═╝░╚═════╝░░░░╚═╝░░░░╚════╝░╚═╝░░░░░╚═╝░░╚═╝░╚════╝░╚═════╝░╚══════╝\e[0m"
 
-echo -e "\e[31mHello, World!\e[0m"
 
-# echo "usage: autoprobe.sh <memory image>"
+echo -e "\e[33m AUTOMATED MEMORY
+                FORENSICS FRAMEWORK\e[0m"
 
-# echo "example: ./autoprobe.sh memory_image.dat"
+echo -e "\e[31m usage: autoprobe.sh <memory image> \e[0m"
+
 
 # echo ""
 
-# #start of autoporbe script
-# echo "******************"
-# echo "**autoprobe has started**"
-# echo "******************"
+
+
+
 # echo ""
 
-# echo "scanning results will be saved in the /results folder"
-# echo "files extracted from memory will be saved to the /exports folder"
+echo -e "\e[33m scanning results will be saved in the /results folder \e[0m"
+echo ""
+echo -e  "\e[33m files extracted from memory will be saved to the /exports folder \e[0m"
 # echo ""
 
 # #setup operations
 
-# mkdir results
-# mkdir exports
+mkdir results
+mkdir exports
 
-# res=results
-# exp=exports
 
-# echo ""
-# echo "identifying the KDBG signature with imageinfo,results pending"
-# echo ""
 
-# date > $res/imageinfo_"$1"\_.txt
-# volatility -f $1 imageinfo | tee -a $res\imageinfo_"$1"\_.txt
+res=results
+exp=exports
 
 # echo ""
-# echo "enter the KDBG signature to use for this memory image,example win2000xbbf1"
-# read kdbg
+
+
 # echo ""
 
-# echo "the operating system profile selected is : --profile="$kdbg
+date > $res/imageinfo_"$1"\_.txt
+volatility -f $1 imageinfo | tee -a $res\imageinfo_"$1"\_.txt
 
-# exec 2>/dev/null
+echo ""
+echo -e " \e[31m enter the KDBG signature to use for this memory image,example win2000xbbf1 \e[0m "
+
+read kdbg
+
+echo ""
+
+echo -e "\e[33m the operating system profile selected is : --profile=\e[0m"$kdbg
+
+exec 2>/dev/null
+
+# automating pslist and psscan
+
+echo -e "listing processes \e[31m{pslist}\e[0m"
+
+volatility -f $1 --profile=$kdbg pslist | tee -a $res/pslist_"$1"\_.txt  
+
+echo ""
+echo -e "listing process \e[31m {psscan}\e[0m "
+
+volatility -f $1 --profile=$kdbg psscan --output=dot --output-file=psscan.dot
+
+xdot psscan.dot
+
+
+# Prompt user to press Enter to continue
+
+echo " press enter to continue"
+
+read -p "Press Enter to continue..."
+
+
