852f2d2e3371517e6e9777b49b9c0e9eb54b43ca
R1V14N <119740534+rivian96@users.noreply.github.com>
2024-05-03 22:24:01 +0530

    Update autoprobe.sh

--- project/autoprobe.sh
+++ project/autoprobe.sh
@@ -11,8 +11,6 @@ echo -e "\e[35m
 ╚═╝░░╚═╝░╚═════╝░░░░╚═╝░░░░╚════╝░╚═╝░░░░░╚═╝░░╚═╝░╚════╝░╚═════╝░╚══════╝\e[0m"
 
 
-echo -e "\e[33m AUTOMATED MEMORY
-                FORENSICS FRAMEWORK\e[0m"
 
 echo -e "\e[31m usage: autoprobe.sh <memory image> \e[0m"
 
@@ -65,17 +63,106 @@ echo -e "listing processes \e[31m{pslist}\e[0m"
 volatility -f $1 --profile=$kdbg pslist | tee -a $res/pslist_"$1"\_.txt  
 
 echo ""
-echo -e "listing process \e[31m {psscan}\e[0m "
-
-volatility -f $1 --profile=$kdbg psscan --output=dot --output-file=psscan.dot
-
-xdot psscan.dot
 
 
 # Prompt user to press Enter to continue
 
-echo " press enter to continue"
+echo -e "\e[94m Press enter to continue\e[0m"
+
+read -p "Press Enter to continue..."
+
+echo ""
+
+echo -e "\e[94manalysing processes from pslist\e[0m"
+
+echo ""
+echo""
+
+echo -e "
+█▀▀ ░▀░ █▀▀▄ █▀▀▀ █░░ █▀▀ ▀▀█▀▀ █▀▀█ █▀▀▄ 　 █▀▀█ █▀▀█ █▀▀█ █▀▀ █▀▀ █▀▀ █▀▀ 
+▀▀█ ▀█▀ █░░█ █░▀█ █░░ █▀▀ ░░█░░ █░░█ █░░█ 　 █░░█ █▄▄▀ █░░█ █░░ █▀▀ ▀▀█ ▀▀█ 
+▀▀▀ ▀▀▀ ▀░░▀ ▀▀▀▀ ▀▀▀ ▀▀▀ ░░▀░░ ▀▀▀▀ ▀░░▀ 　 █▀▀▀ ▀░▀▀ ▀▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ "
+
+echo -e "\e[93m singleton processes are programs or services on acomputer
+that are designed to run only one instance at a time. 
+This means that no matter how many times you try to start the process,
+it will only run one copy\e[0m"
+
+echo ""
+
+echo -e "\e[94mOffset(V)  Name                    PID   PPID   Thds     Hnds   Sess  Wow64 Start                          Exit                          \e[0m"
+
+grep -E -i "(system|winit|lsass|lsm|services|smss|logonUI|rdpclip|wisptis|WaaSMediSvc)" $res/pslist_$1\_.txt > $res/pslist_singletons_$1\_.txt
+
+cat $res/pslist_singletons_$1\_.txt
+
+# Prompt user to press Enter to continue
+
+echo -e "\e[94m Press enter to continue\e[0m"
 
 read -p "Press Enter to continue..."
 
 
+echo -e "\e[93m
+█░░░█ ░▀░ █▀▀▄ █▀▀▄ █▀▀█ █░░░█ █▀▀ 　 █▀▀ █▀▀█ █▀▀█ █▀▀ 
+█▄█▄█ ▀█▀ █░░█ █░░█ █░░█ █▄█▄█ ▀▀█ 　 █░░ █░░█ █▄▄▀ █▀▀ 
+░▀░▀░ ▀▀▀ ▀░░▀ ▀▀▀░ ▀▀▀▀ ░▀░▀░ ▀▀▀ 　 ▀▀▀ ▀▀▀▀ ▀░▀▀ ▀▀▀ 
+
+█▀▀█ █▀▀█ █▀▀█ █▀▀ █▀▀ █▀▀ █▀▀ █▀▀ █▀▀ 
+█░░█ █▄▄▀ █░░█ █░░ █▀▀ ▀▀█ ▀▀█ █▀▀ ▀▀█ 
+█▀▀▀ ▀░▀▀ ▀▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ \e[0m"
+
+echo -e "\e[35mWindows core processes are essential programs or services 
+that are fundamental to the operation of the Windows operating system...\e[0m"
+
+
+echo ""
+
+grep -E -i "(System Idle Process|System|smss|csrss|wininit|services|lsass|svchost|explorer|winlogon|spoolsv|lsass|dllhost|dwm|taskhost|spoolsv|lsaiso|SearchIndexer|RuntimeBroker|SecurityHealthService|NisSrv|WmiPrvSE|PresentationFontCache|csrss|winlogon|LogonUI|fontdrvhost|WUDFHost|WmiApSrv|wininit|conhost|taskhostw|rdpclip|wisptis|WaaSMedicSvc)" $res/pslist_$1\_.txt > $res/pslist_windowscore_$1\_.txt
+
+
+echo -e "\e[94mOffset(V)  Name                    PID   PPID   Thds     Hnds   Sess  Wow64 Start                          Exit                          \e[0m"
+
+cat $res/pslist_windowscore_$1\_.txt
+
+# Prompt user to press Enter to continue
+
+echo -e "\e[94m Press enter to continue\e[0m"
+
+read -p "Press Enter to continue..."
+
+
+echo ""
+
+echo -e "\e[95m
+░█▄─░█ █▀▀█ █▀▀▄ 　 █▀▀ █▀▀█ █▀▀█ █▀▀ 　 █▀▀█ █▀▀█ █▀▀█ █▀▀ █▀▀ █▀▀ █▀▀ █▀▀ █▀▀ 
+░█░█░█ █──█ █──█ 　 █── █──█ █▄▄▀ █▀▀ 　 █──█ █▄▄▀ █──█ █── █▀▀ ▀▀█ ▀▀█ █▀▀ ▀▀█ 
+░█──▀█ ▀▀▀▀ ▀──▀ 　 ▀▀▀ ▀▀▀▀ ▀─▀▀ ▀▀▀ 　 █▀▀▀ ▀─▀▀ ▀▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀\e[0m"
+
+echo -e "\e[34mThese processes are highly\e[0m \e[31mSUSPICIOUS\e[0m "
+
+echo -e "\e[95mBecause these processes are typically user-installed applications, 
+background services, or utilities that provide additional functionalities 
+beyond what the operating system offers by default\e[0m"
+
+
+grep -E -i -v "(System Idle Process|System|smss|csrss|wininit|services|lsass|svchost|explorer|winlogon|spoolsv|lsass|dllhost|dwm|taskhost|spoolsv|lsaiso|SearchIndexer|RuntimeBroker|SecurityHealthService|NisSrv|WmiPrvSE|PresentationFontCache|csrss|winlogon|LogonUI|fontdrvhost|WUDFHost|WmiApSrv|wininit|conhost|taskhostw|rdpclip|wisptis|WaaSMedicSvc)" $res/pslist_$1\_.txt > $res/pslist_Noncore_$1\_.txt
+
+echo -e "\e[94mOffset(V)  Name                    PID   PPID   Thds     Hnds   Sess  Wow64 Start                          Exit                          \e[0m"
+
+cat $res/pslist_Noncore_$1\_.txt
+
+
+# Prompt user to press Enter to continue
+
+echo -e "\e[94m Press enter to continue\e[0m"
+
+read -p "Press Enter to continue..."
+
+
+
+echo -e "\e[31m Now Let's look at the parent child relationship  \e[0m "
+
+volatility -f $1 --profile=$kdbg psscan --output=dot --output-file=psscan.dot
+
+xdot psscan.dot
